810a95f48dde7857cc6adf8a1d54afa4d42a8887
SandeepReddy <74368305+SandeepR3ddy@users.noreply.github.com>
2022-09-16 15:24:24 +0530

    Time: 25 ms (10.27%), Space: 43.4 MB (34.00%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -9,87 +9,76 @@
  */
 class Solution {
     
-    public List<TreeNode> nodeToRootPath(TreeNode root, TreeNode node)
+    public void kdown(TreeNode root, int k, TreeNode block, List<Integer> ans)
     {
-         if(root == null)
-         {
-             return new ArrayList<>();
-         }
-        
-        if(root == node)
-        {
-            
-            List<TreeNode> base = new ArrayList<>();
-            base.add(node);
-            return base;
-        }
-        
-       List<TreeNode> left = nodeToRootPath(root.left,node);
-        
-        if(left.size() != 0)
-        {
-           left.add(root);
-            return left;
-        }
-        
-        
-       List<TreeNode> right = nodeToRootPath(root.right,node);
-        
-        if(right.size() != 0)
-        {
-           right.add(root);
-            return right;
-        }
-        
-        return new ArrayList<>();
-    }
-    
-    public void kdown(TreeNode root,int k, TreeNode block, List<Integer> allNodesAtK)
-    {
-        
-        if(root == null || k < 0 || root == block)
+        if(root == null || k < 0)
         {
             return;
         }
-        
         
         if(k == 0)
         {
-            allNodesAtK.add(root.val);
+          ans.add(root.val);
             return;
         }
         
-        kdown(root.left, k-1, block, allNodesAtK);
+        if(root.left != block)
+        kdown(root.left, k-1, block, ans);
         
-        kdown(root.right, k-1, block, allNodesAtK);
+        if(root.right != block)
+        {
+            kdown(root.right, k-1, block, ans);
+        }
+        
+    }
+    
+    public int distanceK(TreeNode root, TreeNode target, int k , List<Integer> ans)
+    {
+        
+        if(root == null || k < 0)
+        {
+            return 0;
+        }
+        
+        if(root == target)
+        {
+        
+           kdown(root, k, null,  ans);
+            
+            return 1;
+            
+        }
+        
+        
+        int ld = distanceK(root.left, target, k, ans);
+        
+           if(ld > 0)
+           {
+               kdown(root,k-ld,root.left, ans);
+               
+               return ld+1;
+           }
+        
+        int rd = distanceK(root.right, target, k, ans);
+        
+           if(rd > 0)
+           {
+               kdown(root,k-rd,root.right, ans);
+               
+               return rd+1;
+           }
+        
+        return 0;
         
     }
     
     
-    
-    
-    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+         
+        List<Integer> ans = new ArrayList<>();
         
+        distanceK(root, target, k, ans);
         
-        List<TreeNode> allNodes = nodeToRootPath(root, target);
-        
-        
-        List<Integer> res = new ArrayList<>();
-        
-        TreeNode block = null;
-        
-        for(int i = 0;i < allNodes.size();i++)
-        {
-           
-            kdown(allNodes.get(i), k - i, block, res);
-            
-            block = allNodes.get(i);
-             
-        }
-        
-        
-        return res;
-        
+        return ans;
     }
 }
