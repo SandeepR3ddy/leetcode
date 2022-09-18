eeb1c95183800d25168d25e11f390127c4d90672
SandeepReddy <74368305+SandeepR3ddy@users.noreply.github.com>
2022-09-18 08:10:36 +0530

    Time: 133 ms (17.19%), Space: 47.4 MB (51.57%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,50 +10,57 @@
  */
 class Solution {
     
-    public ListNode mergeTwoLists(ListNode l1,ListNode l2)
+    public ListNode merge(ListNode l1, ListNode l2)
     {
         
-        ListNode dummy = new ListNode(-1),prev = dummy,c1 = l1,c2 = l2;
+        ListNode dummy = new ListNode(-1);
+        
+        ListNode prev = dummy, c1 = l1, c2 = l2;
         
         while(c1 != null && c2 != null)
         {
-            if(c1.val < c2.val)
+            
+            if(c1.val <= c2.val)
             {
-                  prev.next = c1;
-                  c1 = c1.next;
+                prev.next = c1;
+                c1 = c1.next;
             }
             else
             {
                 prev.next = c2;
                 c2 = c2.next;
             }
-            prev = prev.next;
+            
+            prev = prev.next;    
         }
         
-        prev.next = (c1 == null) ? c2 : c1;
-        
-        ListNode head = dummy.next;
-        dummy.next = null;
-        return head;
-    }
-    
-    
-    public ListNode mergeKLists(ListNode[] lists, int si,int ei) {
-    
-        if(si >= ei)
+        if(c1 != null)
         {
-            return (si > ei) ? null : lists[si] ;      
+            prev.next = c1;
         }
-     
-        int mid = (si+ei)/2;
+        else
+        {
+            prev.next = c2;
+
+        }
         
-      return mergeTwoLists(mergeKLists(lists,si,mid), mergeKLists(lists,mid+1,ei));
+        return dummy.next;
+        
     }
-        
+    
     
     public ListNode mergeKLists(ListNode[] lists) {
         
-        return mergeKLists(lists,0,lists.length-1);
+        ListNode ans = null;
         
+        for(ListNode node : lists)
+        {
+            
+            ans = merge(ans, node);
+            
+        }
+        
+        
+        return ans;
     }
 }
